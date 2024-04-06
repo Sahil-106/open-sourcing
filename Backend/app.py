ff6281e0d9b35096415d8629e33c14b55e371a5a
Sahil-106 <139853292+Sahil-106@users.noreply.github.com>
2024-04-06 07:59:09 +0000

    commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,13 @@
 import os
 import requests
 from flask import Flask, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 def extract_repo_info(repo):
     return {

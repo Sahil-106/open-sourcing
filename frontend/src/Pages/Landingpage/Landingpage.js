acf78bb1dd81e2b033db9edfc2046676d37c9c32
Pervysage <115885760+Pervysage@users.noreply.github.com>
2024-04-10 17:10:41 +0000

    cleaning files

--- frontend/src/Pages/Landingpage/Landingpage.js
+++ frontend/src/Pages/Landingpage/Landingpage.js
@@ -9,21 +9,21 @@ function Landingpage() {
     useEffect(() => {
         let layouts = ["final", "plain", "columns", "grid"],
 		container = document.querySelector(".container"),
-		curLayout = 0; // index of the current layout
+		curLayout = 0;
 
         function nextState() {
-        const state = Flip.getState(".letter, .for, .gsap", {props: "color,backgroundColor", simple: true}); // capture current state
+        const state = Flip.getState(".letter, .for, .gsap", {props: "color,backgroundColor", simple: true});
         
-        container.classList.remove(layouts[curLayout]); // remove old class
-        curLayout = (curLayout + 1) % layouts.length;   // increment (loop back to the start if at the end)
-        container.classList.add(layouts[curLayout]);    // add the new class
+        container.classList.remove(layouts[curLayout]); 
+        curLayout = (curLayout + 1) % layouts.length; 
+        container.classList.add(layouts[curLayout]);  
 
-        Flip.from(state, { // animate from the previous state
+        Flip.from(state, { 
             absolute: true,
             stagger: 0.07,
             duration: 0.7,
             ease: "power2.inOut",
-            spin: curLayout === 0, // only spin when going to the "final" layout
+            spin: curLayout === 0, 
             simple: true,
             onEnter: (elements, animation) => gsap.fromTo(elements, {opacity: 0}, {opacity: 1, delay: animation.duration() - 0.1}),
             onLeave: elements => gsap.to(elements, {opacity: 0})

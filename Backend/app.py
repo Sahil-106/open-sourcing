d8daf7737b1750ede2baf14ec0266ec4de04ca81
Pervysage <115885760+Pervysage@users.noreply.github.com>
2024-04-15 07:22:23 +0000

    adding gitlab

--- Backend/app.py
+++ Backend/app.py
@@ -16,11 +16,17 @@ def extract_repo_info(repo):
         'url': repo['html_url'],
         'tech_stack': repo['language']
     }
+
+
 access_token=os.environ.get('git_token')
+gitlab_access_token=os.environ.get('lab_token')
+
 
 @app.route('/')
 def index():
     return 'index page'
+
+
 
 @app.route('/repositories')
 def fetch_open_source_repositories():
@@ -43,6 +49,36 @@ def fetch_open_source_repositories():
             return f"Failed to fetch repositories: {response.status_code}", response.status_code
     except requests.exceptions.RequestException as e:
         return f"Error fetching repositories: {e}", 500
+
+
+
+@app.route('/repogitlab')
+def fetch_gitlab_open_source_repositories():
+    headers = {
+        'Private-Token': gitlab_access_token
+    }
+    url = 'https://gitlab.com/api/v4/projects'
+    params = {
+        'per_page': 100,
+        'topics':'open-source',
+        'order_by': 'last_activity_at',
+        
+    }
+    
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            repositories = response.json()
+            repo_info = [{
+                'name': repo['name'],
+                'description': repo['description'],
+                'url': repo['web_url']
+            } for repo in repositories]
+            return jsonify(repo_info)
+        else:
+            return f"Failed to fetch GitLab repositories: {response.status_code}", response.status_code
+    except requests.exceptions.RequestException as e:
+        return f"Error fetching GitLab repositories: {e}", 500
 
 if __name__ == '__main__':
     app.run()
